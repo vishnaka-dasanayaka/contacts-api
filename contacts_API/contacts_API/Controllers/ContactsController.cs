ee0753666878de45195314d395a03d60691591da
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2023-02-28 13:51:14 +0530

    Implement GetContacts method

---
+++ contacts_API/contacts_API/Controllers/ContactsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using contacts_API.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace contacts_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ContactsController : Controller
+    {
+
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());
+        }
+    }
+}
+
