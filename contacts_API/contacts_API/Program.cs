53b40d2ebd4961b95ccfc611181155d2fda437ff
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2023-02-28 13:37:39 +0530

    Inmemory DB

--- contacts_API/contacts_API/Program.cs
+++ contacts_API/contacts_API/Program.cs
@@ -1,4 +1,8 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using contacts_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Inmemory DB
+builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
 
 var app = builder.Build();
 
---
+++ contacts_API/contacts_API/contacts_APIDbContext.cs
@@ -0,0 +1,3 @@
+﻿internal class contacts_APIDbContext
+{
+}
