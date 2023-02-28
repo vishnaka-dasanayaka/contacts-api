44890d725acf3f4074320d25df078f23af5c50c7
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2023-02-28 13:32:23 +0530

    create DbContext class

---
+++ contacts_API/contacts_API/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using contacts_API.Models;
+using System;
+
+namespace contacts_API.Data
+{
+	public class ContactsAPIDbContext: DbContext  
+	{
+		public ContactsAPIDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Contact> Contacts { get; set; }
+	}
+}
+
---
+++ contacts_API/contacts_API/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace contacts_API.Models
+{
+	public class Contact
+	{
+		public Guid Id { get; set; }
+
+		public string FullName { get; set; }
+
+		public string Email { get; set; }
+
+		public long Phone { get; set; }
+
+		public string Address { get; set; }
+
+	}
+}
+
